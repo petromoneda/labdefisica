854f2361b05a789b1c8976dc79c6a9f3bdfc876b
Alvaro Parra <petromoneda@gmail.com>
2025-06-05 03:38:03 -0400

    adicion de texto e imagenes de las secciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
